1b26031ab5a8e7a3bc742c56d332dc44f9b42124
Yaheni1992 <neo_jeka@mail.ru>
2022-02-10 01:56:07 +0300

    дз

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -1,6 +1,37 @@
 package lesson3;
 
+import java.lang.*;
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        Map<String, Integer> hm = new HashMap<>();
+        String[] words = {
+                "звезда", "планета", "воздух",
+                "звезда", "солнце", "земля",
+                "звезда", "солце", "воздух",
+                "птица", "растение", "альфа"
+        };
+
+            for(String e : words) {
+                hm.put(e,hm.getOrDefault(e,0) + 1);
+            }
+
+            System.out.println(hm);
+
+            System.out.println(Arrays.toString((words)));
+
+            Telephone telephonebook = new Telephone();
+            telephonebook.add("Николай", "7894561");
+            telephonebook.add("Николай", "7554561");
+            telephonebook.add("Сярожа", "2587416");
+            telephonebook.add("Игорь", "3259633");
+            telephonebook.add("Игорь", "9959633");
+
+
+            System.out.println("Номер Николая" + telephonebook.get("Николай"));
+            System.out.println("Номер Сярожи" + telephonebook.get("Сярожа"));
+            System.out.println("Номер Игоря" + telephonebook.get("Игорь"));
+        }
     }
-}
--- src/lesson3/Telephone.java
+++ src/lesson3/Telephone.java
@@ -1,5 +1,30 @@
 package lesson3;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
 
-public class Telephone {
+
+ public class Telephone {
+     private Map<String, List<String>> Telephone_hm = new HashMap<>();
+
+    private List<String> phone_number_list;
+
+    public void add(String name, String phone_number) {
+        if (Telephone_hm.containsKey(name)) {
+            phone_number_list = Telephone_hm.get(name);
+            phone_number_list.add(phone_number);
+            Telephone_hm.put(name, phone_number_list);
+        } else {
+            phone_number_list = new ArrayList<>();
+            phone_number_list.add(phone_number);
+            Telephone_hm.put(name, phone_number_list);
+        }
+    }
+
+    public List<String> get(String surname) {
+        return Telephone_hm.get(surname);
+    }
 
 }
+
